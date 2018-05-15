19ab84091a7316d62cef9a2193ab972f51e3949a
khietnt <khietnt@hblab.vn>
2018-05-15 16:03:51 +0700

    delete common folder

--- Modules/Common/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'common', 'namespace' => 'Modules\Common\Http\Controllers'], function()
-{
-    Route::get('/', 'CommonController@index');
-});
--- Modules/Common/Services/Service.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Modules\Common\Services;
-
-abstract class Service
-{
-
-}
